d2d6b16b5872e99298367225458a20e00724d422
Y A S H <87377992+Bcoderx6@users.noreply.github.com>
2021-11-06 12:47:49 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -6,7 +6,7 @@ class Config(Config):
     APP_ID = 7556621
     API_HASH = "0775c99530b756c6f652c8a9ee572aa8"
     OWNER_ID = 1263291513
-    AUTH_CHANNEL = [-1001687642817]
+    AUTH_CHANNEL = [-701661471]
     DESTINATION_FOLDER = "yash unlimited" #Name of your folder read readme(not id of the folder)
     RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0ARrdaM-FI1QPcXFCJDYhpD8hqQyYPhUYMrJLuSiggSEtGz-Zkl3k19iK6PAyhdPwYfXGc2giSaxzGAU1SINDxO43w1HPMq0w30ogtHZuX_dtMkXqbJHKkjnIntz8HNOsl7BJgU6GFID3m1IaCPf4LA_9vyip","token_type":"Bearer","refresh_token":"1//0gmrUoML28wmkCgYIARAAGBASNwF-L9Ir9W2wTuQ_2AuWavGjAZL3ORjhnK5nbdfH-XhV4ATlYB2jjVtMV55q9dSa5VIZURkje8E","expiry":"2021-11-05T12:54:20.8695475+05:30"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
